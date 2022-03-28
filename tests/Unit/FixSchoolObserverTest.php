86129878e2ba9915a1554962d866818a3df6348a
sinan dalal <sinanabom@gmail.com>
2022-03-28 15:07:32 +0300

    bugfix assertSoftDeleted in FixSchoolObserverTest

--- tests/Unit/FixSchoolObserverTest.php
+++ tests/Unit/FixSchoolObserverTest.php
@@ -23,20 +23,27 @@ class FixSchoolObserverTest extends TestCase
 
         $student1 = Student::factory()->create([
             'school_id' => $school->id
-        ]);
+        ])->toArray();
 
         $student2 = Student::factory()->create([
             'school_id' => $school->id
-        ]);
+        ])->toArray();
 
         $student3 = Student::factory()->create([
             'school_id' => $school->id
-        ]);
+        ])->toArray();
 
         $school->delete();
 
-        $this->assertSoftDeleted('students', $student1->toArray());
-        $this->assertSoftDeleted('students', $student2->toArray());
-        $this->assertSoftDeleted('students', $student3->toArray());
+        unset($student1['created_at']);
+        unset($student2['created_at']);
+        unset($student3['created_at']);
+        unset($student1['updated_at']);
+        unset($student2['updated_at']);
+        unset($student3['updated_at']);
+
+        $this->assertSoftDeleted('students', $student1);
+        $this->assertSoftDeleted('students', $student2);
+        $this->assertSoftDeleted('students', $student3);
     }
 }
